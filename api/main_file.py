9a83f2305dc18f976bfd9c9083f29c6b6a134949
itttgg <spdd5724@gmail.com>
2022-08-13 20:41:56 +0300

    Api №3

--- api/main_file.py
+++ api/main_file.py
@@ -12,5 +12,14 @@ def main(text: str) -> str:
 
     if key("хаха"):
         return "Хахахахах"
+    
+    if key('да'):
+        return "Рад, что ты согласен"
+    
+    if key('нет'):
+        return 'Почему нет?'
+    
+    if len(text) > 100:
+        return "Ты решил переписать войну и мир?"
 
     return main_ass(text)

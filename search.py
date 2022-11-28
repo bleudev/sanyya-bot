4d958af5439f6f72b98951266a800e3e7f910f26
itttgg <spdd5724@gmail.com>
2022-11-28 23:20:38 +0300

    Search system №3

--- search.py
+++ search.py
@@ -3,9 +3,18 @@ from bs4 import BeautifulSoup
 from googlesearch import search
 from requests import get
 
+def p(*t):
+    r = ''
+    for i in t:
+        r += i
+    return r
+
+endl = '\n'
+endl2 = endl * 2
+
 def searchh(q: str) -> str:
     url = [i for i in search(q, stop=1, lang='ru') ][0]
     data = get(url).text
     soup = BeautifulSoup(data)
     
-    return soup.title.text
+    return p(soup.title.text, endl2, url)

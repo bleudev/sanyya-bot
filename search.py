e6d777b980b623a253d4d687eb148e02fe7b875f
itttgg <spdd5724@gmail.com>
2022-11-29 08:01:25 +0300

    Search system №4

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 from googlesearch import search
 from requests import get
+from urllib.parse import urlparse
 
 def p(*t):
     r = ''
@@ -14,7 +15,13 @@ endl2 = endl * 2
 
 def searchh(q: str) -> str:
     url = [i for i in search(q, stop=1, lang='ru', country='russia', pause=0) ][0]
+    parsed = urlparse(url)
+    host = parsed.hostname
+    
     data = get(url).text
     soup = BeautifulSoup(data)
     
-    return p(soup.title.text, endl2, url)
+    return p(soup.title.text, endl2, url, endl, host)
+
+def DSearch(url, html):
+    pass

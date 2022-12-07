da2177d3a994049e0540c899d6dd00a754c60453
itttgg <spdd5724@gmail.com>
2022-12-07 11:31:15 +0300

    Update embed

--- dsearch/stackoverflow.py
+++ dsearch/stackoverflow.py
@@ -5,10 +5,13 @@ from bs4 import BeautifulSoup
 def ex(soup: BeautifulSoup, url: str):
     title = soup.find('a', class_='question-hyperlink').get_text()
     post = soup.find('div', class_='s-prose js-post-body')
+    user_details = soup.find('div', class_='user-details')
     
     text = post.get_text()[:300] + '...'
+    user_name = user_details.a.get_text()
 
     emb = Embed(colour=Colour.orange(), title=title, description=text)
+    emb.set_footer(text=user_name)
     
     class StackView(UI.View):
         def __init__(self, *, timeout: float = 180):

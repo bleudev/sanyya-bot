bf4d22532ae6e3d0efb8f694b5a6f7e916aaa4e4
itttgg <spdd5724@gmail.com>
2022-12-07 11:37:15 +0300

    Edit footer

--- dsearch/stackoverflow.py
+++ dsearch/stackoverflow.py
@@ -6,12 +6,16 @@ def ex(soup: BeautifulSoup, url: str):
     title = soup.find('a', class_='question-hyperlink').get_text()
     post = soup.find('div', class_='s-prose js-post-body')
     user_details = soup.find('div', class_='user-details')
+    quest_time = soup.find('div', class_='user-action-time')
     
     text = post.get_text()[:300] + '...'
     user_name = user_details.a.get_text()
+    time = quest_time.span.get_text()
+    
+    footer = f'{user_name} задал этот вопрос {time}'
 
     emb = Embed(colour=Colour.orange(), title=title, description=text)
-    emb.set_footer(text=user_name)
+    emb.set_footer(text=footer)
     
     class StackView(UI.View):
         def __init__(self, *, timeout: float = 180):
